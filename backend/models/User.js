0c22cd193495790c09dd117953225809e5808bb1
SASOFFICIAL <167342543+AmlanAmrutansu@users.noreply.github.com>
2025-05-23 19:14:32 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
-  password: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, select: false },
   googleId: String,
   avatar: String,
   bio: String,
